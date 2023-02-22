a801039bcd304dd1cfcf5ad91b596784a4034fd2
sgnilo <xujiangpingtdb@icloud.com>
2023-02-22 22:16:17 +0800

    chore: use md5 instead of autoincrement id to mark previewBlock

--- src/markdown.ts
+++ src/markdown.ts
@@ -5,6 +5,7 @@
 
 import fs from 'fs';
 import path from 'path';
+import crypto from 'crypto';
 import {marked} from 'marked';
 import {parseCodeLang} from './query';
 
@@ -65,6 +66,8 @@ function init(options: CompileOptions) {
     previewBlocks = new Map();
 }
 
+const md5 = (content: string) => crypto.createHash('md5').update(content).digest('hex');
+
 const renderer = {
     code(code: string, infostring: string) {
         const codeEsc = code
@@ -97,11 +100,11 @@ const renderer = {
                     type: 'css'
                 });
             });
-
-            const entryTag = `preview-block-${index}`;
-            const entryVar = `PreviewBlock${index}`;
+            const id = `${index}${md5(code)}`;
+            const entryTag = `preview-block-${id}`;
+            const entryVar = `PreviewBlock${id}`;
             const mapKeyEntry = `${entryVar}.vpms`;
-            const mapKeyComponent = `Component${index}.vpms`;
+            const mapKeyComponent = `Component${id}.vpms`;
             // /src/markdown/html.md.PreviewBlock1.vpms
             const entryRequest = `${file}.${mapKeyEntry}`;
             // /src/markdown/html.md.Component1.vpms
@@ -111,7 +114,7 @@ const renderer = {
                 component: `'${entryTag}': ${entryVar}`
             });
             previewBlocks.set(mapKeyEntry, getTemplate({
-                id: index,
+                id,
                 code: codeEsc,
                 filepath: file,
                 componentRequest,
